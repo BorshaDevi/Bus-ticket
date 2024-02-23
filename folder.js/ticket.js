f0409f0e0970f07c0d6a844d4e38f357cb948cc6
Borshadevi <borshadevi27@gmail.com>
2024-02-23 16:37:16 +0600

    add text

---
+++ folder.js/ticket.js
@@ -0,0 +1,8 @@
+function getAiPoribon(){
+    const bannar=document.getElementById('banner')
+    bannar.classList.add('hidden')
+     const coupon=document.getElementById('coupon')
+     coupon.classList.add('hidden')
+    const AiPoribon=document.getElementById('Ai-poribon')
+    AiPoribon.classList.remove('hidden')
+}
