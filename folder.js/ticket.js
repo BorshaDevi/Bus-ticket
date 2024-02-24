37e6b8956df7740c727820bc01b066a7c0f605e0
Borshadevi <borshadevi27@gmail.com>
2024-02-24 10:38:22 +0600

    add text

--- folder.js/ticket.js
+++ folder.js/ticket.js
@@ -13,21 +13,39 @@ function getNumberInter(elementId){
 }
 const seatNumber=getNumberInter('seat-number')
 const seatsLeft=getNumberInter('seats-left')
+const takaId=getNumberInter('takaId')
+
+
+
+
 seatnum=0
 const seatBtn=document.getElementsByClassName('seat-btn')
 for(let seat of seatBtn){
-    
-    seat.addEventListener('click',function(event){ 
+    seat.addEventListener('click',function(){ 
         if(seatnum<4===true){
+            seatnum=1+seatnum
             seat.classList.add('seatbtn')
             seat.classList.add('text-white')
             seat.target.setAttribute('disabled',false)
             seat.classList.remove('seatbtn')
-            seatnum=seatnum+1
 
-        }
+            // const seatLeftNum=seatsLeft-1
+            // console.log(seatLeftNum)
 
-        
+            const selectDiv=document.createElement('div')
+            selectDiv.classList.add('setected-filedid')
+            const p1=document.createElement('p')
+            p1.innerText=seat.innerText
+            const p2=document.createElement('p')
+            p2.innerText='Economy'
+            const p3=document.createElement('p')
+            p3.innerText=takaId
+            selectDiv.append(p1)
+            selectDiv.append(p2)
+            selectDiv.append(p3)
+            const setectedFiledId=document.getElementById('setected-filed-id')
+            setectedFiledId.append(selectDiv)
+        } 
     })
 }
 
